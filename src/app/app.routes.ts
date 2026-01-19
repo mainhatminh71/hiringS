72abc6fceeb3a19a31b0bd48780cfbae8df2dd00
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-19 09:09:25 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { CareersComponent } from './careers/careers.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/careers', pathMatch: 'full' },
-  { path: 'careers', component: CareersComponent }
 ];
