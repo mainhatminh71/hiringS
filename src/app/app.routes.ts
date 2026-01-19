9c15aa4864b185a5555e1bc2bd21918caa52e682
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-19 09:15:50 +0700

    Add route for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { CareersComponent } from './pages/careers.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/careers', pathMatch: 'full' },
+  { path: 'careers', component: CareersComponent },
 ];
