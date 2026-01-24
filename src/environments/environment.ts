741529b8cf9296a5ce72f41f861fdcaea1a9395f
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-24 18:56:50 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
-import { environmentSecret } from "./environment.secret";
 
 export const environment = {
     production: false,
     firebase: {
-      apiKey: environmentSecret.firebase.apiKey ||
+      apiKey: 
   'FIREBASE_API_KEY',
       authDomain: "hirings-a76b9.firebaseapp.com",
       projectId: "hirings-a76b9",
@@ -11,9 +10,9 @@ export const environment = {
       messagingSenderId: "326780634372",
       appId: "1:326780634372:web:5a4ae3011e8b5bf479bc51",
       measurementId: "G-5KB91QBCP2",
-      firebaseEmail:   environmentSecret.firebase.firebaseEmail ||
+      firebaseEmail:  
 'FIREBASE_EMAIL',
-      firebasePassword: environmentSecret.firebase.firebasePassword ||  'FIREBASE_PASSWORD'
+      firebasePassword:  'FIREBASE_PASSWORD'
     }
   };
 export const testCustomerId = 'tech-v4';
