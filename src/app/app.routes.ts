3b14a9a41aff091caf47ca19195909513b0af40f
Mai Nhật Minh <nhatmoon71@gmail.com>
2026-01-28 10:40:16 +0700

    Delete all things related to form generation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { formConfigResolver } from '../lib/core/resolver/form-config.resolver';
 import { ApplicationSelectionComponent } from './pages/application-selection/application-selection.component';
-import { FormGenerationComponent } from './pages/form-generation/form-generation.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/careers', pathMatch: 'full' },
   { path: 'careers', component: CareersComponent },
@@ -16,5 +15,4 @@ export const routes: Routes = [
   resolve: { formConfig: formConfigResolver } ,
   data: { formType: 'signUp' }},
   {path: 'application-selection', component: ApplicationSelectionComponent,},
-  {path: 'form-generation', component: FormGenerationComponent,},
 ];
